7c4fc9d98417f72ccde43fa47919e52e7534ff99
Chris Siannas <xrsia@otenet.gr>
2022-04-04 12:44:00 +0300

    command line flags - default values

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -47,7 +47,8 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
+		x := 405
+		http.Error(w, "Method Not Allowed", x)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
